a0355d7c350bd354bc813646a88e40d2b546874a
mayudev <99561108+mayudev@users.noreply.github.com>
2022-03-29 12:47:43 +0200

    fix: animated discord emoji

--- app/util/regex.ts
+++ app/util/regex.ts
@@ -1,6 +1,6 @@
 // Yes, I did look at Jan's DiscordBridge code, but I tried to make those pattern myself. Really. Trust me.
 
-export const DiscordEmojiPattern = /<:(?<name>.+?):(?<id>[0-9]{1,22})>/g;
+export const DiscordEmojiPattern = /<(:|a:)(?<name>.+?):(?<id>[0-9]{1,22})>/g;
 export const DiscordPingPattern = /<(@|@!)(?<id>[0-9]{1,22})>/g;
 export const DiscordChannelPattern = /<#(?<id>[0-9]{1,22})>/g;
 
